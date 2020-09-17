db954ea05d2c49940d22e773dc611941446acc17
torco34 <torcoromaarias@gimail.com>
2020-09-17 13:34:45 -0500

    Cambiado el click del hader por for

--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
 let playlists = document.getElementById('top_playlists')
-let artists = document.getElementById('top_artists')
+let artista = document.getElementById('top_artists')
 let videos = document.getElementById('top_videos')
-let contenedor = document.getElementById('container')
+let lista = document.getElementById('lista')
 
 
 
-console.log(playlists, artists, videos, contenedor )
-function selecionDePestana(e){
-	
-	alert('hola soy yo!')
+console.log(lista)
+// console.log(playlists, artists, videos, contenedor )
 
+function  selecionar(){
+	// alert('artista favorito')
+	console.log('artista favorito')
 }
-contenedor.addEventListener('click', selecionDePestana)
-selecionDePestana()
+
+
+
+// listaPrincipal()
+// seleccionVideos()
+//  selecionArtistas()
+// playlists.addEventListener('click', listaPrincipal)
+// artista.addEventListener('click', selecionArtistas)
+// videos.addEventListener('click', seleccionVideos)
+ for(let i = 0; i<=lista.children.length -1; i++){
+ 	lista.children[i].addEventListener('click', selecionar)
+ }
